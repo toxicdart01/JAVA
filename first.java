b3b5043828ce3f0008f437b5bb29d89efc6e1977
toxicdart01 <tejast96k@gmail.com>
2024-02-15 15:19:24 +0530

    new file:   first.java 	new file:   hello.java

---
+++ first.java
@@ -0,0 +1,22 @@
+
+
+public class first {
+    public static void main(String []args){
+        int a=30,b=11,c;
+        System.out.println("Value of A=30 & B=15 ");
+
+        //type(a)
+        c=a+b;
+        System.out.println("The Addition is = "+c);
+    
+        //type(b)
+        System.out.println("The Suntraction is = "+ (a-b));
+        
+        System.out.println("The Multiplication is = "+(a*b));
+
+        System.out.println("The Division will be = "+a/b);
+
+        System.out.println("Module operator = "+a%b);   //Remainder after division.
+
+    }
+}
---
+++ hello.java
@@ -0,0 +1,17 @@
+
+// Enter main class name 'hello' same as program name 
+class hello {
+
+    //Declare the syntax of main method(in java function is called as method)
+    //It should available publically.
+    //It should be of static type
+    //void is return type
+    //main method is same as main function in c/c++
+    //It takes String arguement in notepad while running program in command prompt
+    public static void main(String []args){
+
+        //Use it to print message
+        System.out.println("Hello this is My First Java Program ");
+
+    }
+}
